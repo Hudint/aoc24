48ee0124c735414106a4d11cbe937687492ec0d2
hudint <admin@hudint.de>
2024-12-10 11:46:45 +0100

    day10

---
+++ src/day10/index.ts
@@ -0,0 +1,50 @@
+import ArrayUtils from "../utils/arrayUtils";
+import FieldUtils, { Direction, DirectionsOffsets, Position } from "../utils/fieldUtils";
+import FileUtils from "../utils/fileUtils";
+import Utils from "../utils/utils";
+
+
+const prep = (input: string) => {
+    const lines = Utils.splitToLines(input)
+    const field = FieldUtils.linesToField(lines)
+
+    function scoreTrailHead(start: Position, distinct: boolean){
+        if(field[start.x][start.y] !== 0)
+            return 0;
+
+        let done: Position[] = [];
+        let curr: Position[] = [start]
+        let count = 0;
+
+        while(curr.length !== 0) {
+            done.push(...curr)
+            done = ArrayUtils.onlyUnique(done)
+
+            curr = curr.flatMap(pos => FieldUtils.getDirections()
+            .map(d => FieldUtils.add(pos, DirectionsOffsets[d]))
+            .filter(np => FieldUtils.validPosition(field, np) && field[np.x][np.y] === (field[pos.x][pos.y] + 1) && Array.from(done).findIndex(p => Utils.objectEquals(np, p)) === -1))
+            if(!distinct)
+                curr = ArrayUtils.onlyUnique(curr)
+            count += curr.filter(c => field[c.x][c.y] === 9).length
+        }
+        return count
+    }
+
+    return {field, scoreTrailHead}
+}
+
+
+
+const solvePart1 = (input: string) => {
+    const {field, scoreTrailHead} = prep(input)
+    return field.map((r,x) => r.map((c,y) => scoreTrailHead({x,y}, false))).map(r => r.reduce(Utils.adding)).reduce(Utils.adding)
+}
+
+const solvePart2 = (input: string) => {
+    const {field,scoreTrailHead} = prep(input)
+    return field.map((r,x) => r.map((c,y) => scoreTrailHead({x,y}, true))).map(r => r.reduce(Utils.adding)).reduce(Utils.adding)
+
+}
+
+console.log(`Solution for Part 1:`, solvePart1(FileUtils.readFile(false)))
+console.log(`Solution for Part 2:`, solvePart2(FileUtils.readFile(false)))
--- src/utils/fieldUtils.ts
+++ src/utils/fieldUtils.ts
@@ -1,12 +1,48 @@
+import { Dir } from "fs"
 
 export interface Position {
     x: number,
     y: number
 }
 
-export default class FieldUtils {
+export enum Direction{
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+} 
 
+export const DirectionsOffsets: {[key in Direction]: Position} = {
+    [Direction.UP]: {x: -1, y: 0},
+    [Direction.RIGHT]: {x: 0, y: 1},
+    [Direction.DOWN]: {x: 1, y: 0},
+    [Direction.LEFT]: {x: 0, y: -1},
+}
+
+export default class FieldUtils {
+    
     private constructor() {}
+
+    static getDirections() : Direction[] {
+        return Object.keys(Direction)
+            .map(d => Number.parseInt(d))
+            .filter(d => !isNaN(d))
+    }
+
+    static operation(pos1: Position, pos2: Position, op: (n1: number, n2: number) => number): Position {
+        return {
+            x: op(pos1.x, pos2.x),
+            y: op(pos1.y, pos2.y)
+        }
+    }
+
+    static add(pos1: Position, pos2: Position){
+        return this.operation(pos1, pos2, (n1,n2) => n1+n2)
+    }
+
+    static linesToField(lines: string[]) {
+        return lines.map(row => row.split("").map(n => Number.parseInt(n)))
+    }
 
     static getPositionsFromField<T>(field: T[][], search: T) {
         let result: Position[] = []
